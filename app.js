adb2e9e4bcb340db8d53705cd4a7441cef48cdd2
sturpin <turpiniko@gmail.com>
2021-08-31 10:58:56 +0200

    Salida modificada

--- app.js
+++ app.js
@@ -5,5 +5,5 @@ require('colors');
 console.clear();
 
 crearArchivo( argv.b, argv.l, argv.h )
-    .then( nombreArchivo => console.log( nombreArchivo.yellow, 'creada!') )
+    .then( nombreArchivo => console.log('Archivo', nombreArchivo.yellow, 'creado correctamente!') )
     .catch( err => console.log( err ));
